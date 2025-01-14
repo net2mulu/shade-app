65722227103b6c645ab9296e1f2ec7483156fe96
kb <kbleulseged@gmail.com>
2025-01-14 14:45:37 +0300

    DONE: refactor register shade form

--- src/apollo/base_data/query.js
+++ src/apollo/base_data/query.js
@@ -45,15 +45,6 @@ export const COMBINED_SELECT_BASE_DATA = gql`
   }
 `;
 
-export const GET_REGIONS = gql`
-  query MyQuery {
-    base_regions {
-      id
-      namejson
-    }
-  }
-`;
-
 export const GET_ZONES = gql`
   query MyQuery($where: base_zone_bool_exp) {
     base_zone(where: $where) {
@@ -82,90 +73,9 @@ export const GET_KEBELES = gql`
   }
 `;
 
-export const GET_WOREDA = gql`
-  query MyQuery {
-    base_woreda {
-      id
-      namejson
-    }
-  }
-`;
-
-export const GET_SECTORS = gql`
-  query MyQuery {
-    base_sectors {
-      id
-      namejson
-    }
-  }
-`;
-
 export const GET_SHED_TYPES = gql`
   query MyQuery {
     base_shed_types {
-      id
-      name_json
-    }
-  }
-`;
-
-export const GET_SERVICES_TYPES = gql`
-  query MyQuery {
-    base_service_types {
-      id
-      name_json
-    }
-  }
-`;
-
-export const GET_BUILT_BY = gql`
-  query MyQuery {
-    base_built_by {
-      id
-      name_json
-    }
-  }
-`;
-
-export const GET_CONSTRUCTION_LEVELS = gql`
-  query MyQuery {
-    base_construction_levels {
-      id
-      name_json
-    }
-  }
-`;
-
-export const GET_CONSTRUCTION_TYPES = gql`
-  query MyQuery {
-    base_construction_types {
-      id
-      name_json
-    }
-  }
-`;
-
-export const GET_CONSTRUCTION_NOT_TRANSFERRED_REASON = gql`
-  query MyQuery {
-    base_not_transferred_reasons {
-      id
-      name_json
-    }
-  }
-`;
-
-export const GET_CONSTRUCTION_STOPPED_REASON = gql`
-  query MyQuery {
-    base_construction_stopped_reasons {
-      id
-      name_json
-    }
-  }
-`;
-
-export const GET_FLOORS = gql`
-  query MyQuery {
-    base_number_of_floors {
       id
       name_json
     }
